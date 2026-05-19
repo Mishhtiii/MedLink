eee1d40731e81d12778b200512b8f117ffb8feb0
Mehak <mehakgarg598@gmail.com>
2026-05-19 22:36:49 +0530

    changed user and doctor ui

--- api/dataLoader.js
+++ api/dataLoader.js
@@ -17,14 +17,14 @@ const SurgicalDevice = mongoose.model('SurgicalDevice', surgicalDeviceSchema);
 
 const loadMedicines = async () => {
   try {
-    const cacheKey = 'medicines';
-    const cachedData = await redisClient.get(cacheKey);
-    if (cachedData) {
-      console.log('Loaded medicines from cache');
-      return JSON.parse(cachedData);
-    }
+    // const cacheKey = 'medicines';
+    // const cachedData = await redisClient.get(cacheKey);
+    // if (cachedData) {
+    //   console.log('Loaded medicines from cache');
+    //   return JSON.parse(cachedData);
+    // }
     const data = await Ayurvedic.find({}).lean();
-    await redisClient.setEx(cacheKey, 3600, JSON.stringify(data)); // cache for 1 hour
+    // await redisClient.setEx(cacheKey, 3600, JSON.stringify(data)); // cache for 1 hour
     console.log('Loaded medicines:', data.length);
     return data;
   } catch (err) {
